c481f81640774ad0dd5edff4dc9162028fc05832
gitlab-init <sebastien@cheminel.com>
2020-06-19 23:44:38 +0200

    uuid dans script installation

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aptitude install docker.io docker-compose
+aptitude install uuid docker.io docker-compose
 useradd -d /home/$1 -m -p $(uuid) -s /bin/bash gitlab
 usermod -aG docker gitlab 
 
